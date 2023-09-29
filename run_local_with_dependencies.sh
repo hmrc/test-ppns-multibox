ba975d21e7e50900082aa976b96bb73e27622913
John Green <john.green@digital.hmrc.gov.uk>
2023-09-29 11:26:02 +0100

    APIS-6448: Fix the dependency to PPNS when running locally

--- run_local_with_dependencies.sh
+++ run_local_with_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sm2 --start AUTH OAUTH THIRD_PARTY_APPLICATION THIRD_PARTY_DELEGATED_AUTHORITY
-sm2 --start PUSH_PULL_NOTIFICATIONS_API --appendArgs '{"PUSH_PULL_NOTIFICATIONS_API": ["-Dallowlisted.useragents.0=test-ppns-multibox"]}'
+sm2 --start PUSH_PULL_NOTIFICATIONS_API --appendArgs '{"PUSH_PULL_NOTIFICATIONS_API": ["-Dallowlisted.useragents.0=api-subscription-fields","-Dallowlisted.useragents.1=test-ppns-multibox"]}'
 
 ./run_local.sh
