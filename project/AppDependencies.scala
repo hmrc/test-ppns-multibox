da1538a9c3e72be52301f7dcf2e531f5ca8095d9
John Green <john.green@digital.hmrc.gov.uk>
2024-01-31 17:38:22 +0000

    APIS-6744: Remove transitive dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -16,9 +16,6 @@ object AppDependencies {
   val test = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion,
     "org.mockito"             %% "mockito-scala-scalatest"    % "1.17.29",
-    "org.scalatest"           %% "scalatest"                  % "3.2.17",
-    "com.vladsch.flexmark"     % "flexmark-all"               % "0.62.2",
-    "org.scalatestplus.play"  %% "scalatestplus-play"         % "5.1.0",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"    % hmrcMongoVersion
   ).map(_ % "test, it")
 }
